8727982cfd961683183dbe8a4bcc3fdfc3f2b423
Italo Santos <italosantoscont@gmail.com>
2026-02-02 08:49:49 -0300

    build: initial project setup

---
+++ src/main/java/com/italo/copiavideo/CopiaVideoApplication.java
@@ -0,0 +1,13 @@
+package com.italo.copiavideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopiaVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopiaVideoApplication.class, args);
+	}
+
+}
