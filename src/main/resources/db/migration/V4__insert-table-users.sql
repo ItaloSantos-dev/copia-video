ae7a2216b2502538b88e42254c9b39dc1be89a31
Italo Santos <italosantoscont@gmail.com>
2026-02-03 15:42:52 -0300

    feat: create migration for insert user admin

---
+++ src/main/resources/db/migration/V4__insert-table-users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, name, email, password, role) VALUES
+('32f8f840-117e-4f9d-bcfc-104be5db892e', 'admin', 'admin@.com', '$2a$10$wHbheI2D8VC4Uz.6QGq.XOsyLXobC./cDJv8NR4/dPRFpuHd9PY/m', '0');
