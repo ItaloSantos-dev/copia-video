a8ba024b6b68e1674ecb533a0caf779efab0968e
Italo Santos <italosantoscont@gmail.com>
2026-02-03 09:56:02 -0300

    feat(User): create in UserRepository function findByEmail

--- src/main/java/com/italo/copiavideo/repository/UserRepository.java
+++ src/main/java/com/italo/copiavideo/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.italo.copiavideo.repository;
 
 import com.italo.copiavideo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
+
 }
